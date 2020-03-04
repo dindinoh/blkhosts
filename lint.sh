76b9c4555c7a7f0a9307ecb3678d14f410b764e0
Dandylejon <dandylejon@kejsarkungen.se>
2020-03-04 20:20:47 +0100

    witch

--- lint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-clear
-
-echo -e "### cljfmt" ; lein cljfmt check && echo "\n### bikeshed" ; lein bikeshed -v && echo "\n### eastwood" ; lein eastwood
--- test/blkhosts/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns blkhosts.core-test
-  (:require [clojure.test :refer :all]
-            [blkhosts.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
